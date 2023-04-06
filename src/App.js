3fa5ec6acbfa0afde711f51f36b1f58cf13e6d1f
cooper <tokyolabs@protonmail.com>
2023-04-06 15:44:29 -0400

    added publish event func

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import logo from './banana.svg';
 import './App.css';
-import { generatePrivateKey, getPublicKey, relayInit } from 'nostr-tools';
+import {
+  generatePrivateKey,
+  getEventHash,
+  getPublicKey,
+  relayInit,
+  signEvent
+} from 'nostr-tools';
 import { useState, useEffect } from 'react';
 
 function App() {
   const [sk, setSk] = useState(generatePrivateKey());
   const [pk, setPk] = useState(getPublicKey(sk));
   const [relay, setRelay] = useState(null);
+  const [pubStatus, setPubStatus] = useState('');
 
   useEffect(() => {
     const connectRelay = async () => {
-      const relay = relayInit('wss://relay.damus');
+      const relay = relayInit('wss://relay.damus.io');
       await relay.connect();
 
       relay.on('connect', () => {
@@ -23,6 +30,28 @@ function App() {
     connectRelay();
   });
 
+  var event = {
+    kind: 1,
+    pubkey: pk,
+    created_at: Math.floor(Date.now() / 1000),
+    tags: [],
+    content: 'Testing nostr from a react client'
+  };
+
+  event.id = getEventHash(event);
+  event.sig = signEvent(event, sk);
+
+  const publishEvent = event => {
+    const pub = relay.publish(event);
+
+    pub.on('ok', () => {
+      setPubStatus('the event is published');
+    });
+    pub.on('failed', reason => {
+      setPubStatus(`failed to publish message ${reason}`);
+    });
+  };
+
   return (
     <div className='App'>
       <header className='App-header'>
@@ -32,6 +61,10 @@ function App() {
         <p>public key: {pk}</p>
         {relay ? <p>Connected to {relay.url}</p> : <p>Could not connect</p>}
       </header>
+      <div>
+        <button onClick={() => publishEvent(event)}>Publish event</button>
+        <p>Publish status: {pubStatus}</p>
+      </div>
     </div>
   );
 }
