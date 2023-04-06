3095559eac71f9ea673933c2cf232ccb161e36ba
cooper <tokyolabs@protonmail.com>
2023-04-06 13:26:09 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import logo from './banana.svg';
 import './App.css';
+import { generatePrivateKey, getPublicKey, relayInit } from 'nostr-tools';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [sk, setSk] = useState(generatePrivateKey());
+  const [pk, setPk] = useState(getPublicKey(sk));
+  const [relay, setRelay] = useState(null);
+
+  useEffect(() => {
+    const connectRelay = async () => {
+      const relay = relayInit('wss://relay.damus');
+      await relay.connect();
+
+      relay.on('connect', () => {
+        setRelay(relay);
+      });
+      relay.on('error', () => {
+        console.log('Failed to connect');
+      });
+    };
+    connectRelay();
+  });
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
         <h1>nosrtcli</h1>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>private key: {sk}</p>
+        <p>public key: {pk}</p>
+        {relay ? <p>Connected to {relay.url}</p> : <p>Could not connect</p>}
       </header>
     </div>
   );
