1ee52aa10c18630615975cd17df555505f3e0e7f
cooper <tokyolabs@protonmail.com>
2023-04-09 07:41:00 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-import SignIn from './components/signin';
-import SignUp from './components/signup';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='SignIn' element={<SignIn />} />
-        <Route path='SignUp' element={<SignUp />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <p>Copyright &copy; 2023</p>
+      <Link to='/about' className='link'>
+        About
+      </Link>
+    </footer>
+  );
+};
+
+export default Footer;
